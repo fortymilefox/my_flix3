8ffaa16324aedcd42dec01f01b3e45c7a162b314
fortymilefox <59665281+fortymilefox@users.noreply.github.com>
2020-05-21 12:41:22 -0400

    changed cors for local react app

--- index.js
+++ index.js
@@ -33,8 +33,7 @@ app.use(bodyParser.json());
 //Authentication
 let auth = require('./auth')(app);
 
-let allowedOrigins = ['http://localhost:8080', 'https://myflix-1.herokuapp.com', 'http://localhost:1234'];
-
+let allowedOrigins = ['*']
 app.use(cors({
   origin: (origin, callback) => {
     if (!origin) return callback(null, true);
